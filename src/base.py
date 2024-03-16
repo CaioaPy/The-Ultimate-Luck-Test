cb1aa379a65d057693a065926e9bf5b80d5f7ab7
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-03-16 09:50:28 -0300

    Update base.py

--- src/base.py
+++ src/base.py
@@ -3,7 +3,5 @@
 # Must
 
 # SAVE INFO 
-# Stats update every x minutes (?)
-# GUI home/rng> upgrade(?) up> leaderboard
 # Configure RNG - done
 # List of rollable things in RNG and their respective % / chances - done 
