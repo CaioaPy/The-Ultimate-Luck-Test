3af83f7eb891312422ea339c2bed7e7b08d9c05d
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-18 21:35:29 -0300

    Update base.py

--- src/base.py
+++ src/base.py
@@ -1,5 +1,4 @@
 # Project RNG 
 
-# Functional Tkinter
 # Configure RNG - done
-# List of rollable things in RNG and their respective % / chances - done 
+# List of rollable things in RNG and their respective % / chances - done
