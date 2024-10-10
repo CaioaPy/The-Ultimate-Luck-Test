bde90b168d37eff4b70bd216d8419be2c8256d53
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-10-10 08:59:01 -0300

    updated function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-function scale_roll(min = 1, max = 10, skew = 2) {
+const myh1 = document.getElementById("myh1");
+roll_button = document.getElementById("roll")
+roll_button.addEventListener("click", function scale_roll(min = 1, max = 10, skew = 2) {
     const random = Math.random();
 
     if (random < 1 / max) {
@@ -8,8 +10,4 @@ function scale_roll(min = 1, max = 10, skew = 2) {
     const adjusted = Math.pow(Math.random(), skew);
     const roll = Math.floor(min + adjusted * (max - min + 1));
     myh1.innerHTML = roll;
-}
-
-const myh1 = document.getElementById("myh1");
-roll_button = document.getElementById("roll")
-roll_button.addEventListener("click", scale_roll());
+});
