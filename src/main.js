8a606910fa8effdc61c806085421dad0bed25dff
yatomikazuchi <caioevilabner@gmail.com>
2024-10-14 20:53:20 -0300

    new random function formula

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const roll_button = document.getElementById("roll");
 function scale_roll(min = 1, max = 10, skew = 2) {
     const random = Math.random();
 
-    if (random < 1 / max) {
-        return scale_roll(min, max * 100, skew);
+    if (random < 10 / max) {
+        return scale_roll(min, max * 10, skew);
     }
 
     const adjusted = Math.pow(Math.random(), skew);
