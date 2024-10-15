7e8d50d43b03cca8f914b88b559f7bd50c1155f9
yatomikazuchi <caioevilabner@gmail.com>
2024-10-14 21:54:02 -0300

    implemented new variable

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const myh1 = document.getElementById("myh1");
 const roll_button = document.getElementById("roll");
 
-function scale_roll(min = 1, max = 10, skew = 2) {
+function scale_roll(min = 1, max = 10, skew = 2, next=5) {
     const random = Math.random();
 
-    if (random < 10 / max) {
-        return scale_roll(min, max * 10, skew);
+    if (random < next / max) {
+        return scale_roll(min, max * 10, skew, next += 10);
     }
 
     const adjusted = Math.pow(Math.random(), skew);
