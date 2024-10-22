27bf84a8a11037a18c5172fb0178b76fa54885d7
yatomikazuchi <caioevilabner@gmail.com>
2024-10-21 23:19:30 -0300

    format save-roll.js

--- api/save-roll.js
+++ api/save-roll.js
@@ -1,10 +1,26 @@
-export default function handler(req, res) {
+import mysql from 'mysql2/promise';
+
+const connectionConfig = {
+    host: 'sql5.freesqldatabase.com',
+    user: 'sql5739727',
+    password: 'hWr2gWUVle',
+    database: 'sql5739727'
+};
+
+export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { username, roll } = req.body;
-
-        console.log(`Username: ${username}, Roll: ${roll}`);
-
-        return res.status(200).json({ message: 'Roll saved!', username, roll });
+        
+        try {
+            const connection = await mysql.createConnection(connectionConfig);
+            const sql = 'INSERT INTO rolls (username, roll) VALUES (?, ?)';
+            await connection.execute(sql, [username, roll]);
+            await connection.end();
+            return res.status(200).send('Roll saved!');
+        } catch (error) {
+            console.error('Error saving data:', error);
+            return res.status(500).send('Error saving data');
+        }
     } else {
         return res.status(405).json({ message: 'Method not allowed' });
     }
